5c20be9e4ce3666f80c41d57b91edf03fd9fc911
gokulr-05 <gokulravivcr05@gmail.com>
2022-05-20 22:28:47 +0530

    finished

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,45 @@
 import classes from "./Counter.module.css";
-
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  let dispatchFunc = useDispatch();
+  let counter = useSelector((state) => {
+    return state.counter;
+  });
+
+  let showCounterr = useSelector((state) => {
+    return state.showCounter;
+  });
+
+  let inc = (e) => {
+    dispatchFunc({ type: "INC" });
+  };
+  let dec = (e) => {
+    dispatchFunc({ type: "DEC" });
+  };
+
+  let incVal = (e) => {
+    dispatchFunc({ type: "INCVAL", value: 5 });
+  };
+
+  let toggleCounterHandler = () => {
+    dispatchFunc({ type: "showcounter" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounterr && <div className={classes.value}>{counter}</div>}
+      <div className="my-3 d-flex align-items-center gap-3 justify-content-center">
+        <button onClick={inc} className="btn btn-primary text-capitalize">
+          increment
+        </button>
+        <button onClick={incVal} className="btn btn-primary text-capitalize">
+          increase by 5
+        </button>
+        <button onClick={dec} className="btn btn-primary text-capitalize">
+          decrement
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,24 @@ import { createStore } from "redux";
 
 let reducerFunc = (state, action) => {
   if (action.type === "INC") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   } else if (action.type === "DEC") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  } else if (action.type === "INCVAL") {
+    return {
+      counter: state.counter + action.value,
+      showCounter: state.showCounter,
+    };
+  } else if (action.type === "showcounter") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
 
   return state;
 };
 
-let store = createStore(reducerFunc);
+let store = createStore(reducerFunc, { counter: 0, showCounter: true });
 
 export default store;
